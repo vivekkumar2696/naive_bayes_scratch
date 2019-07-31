21f81c16f5686873f05cdf0ca6bd09d4ee682c09
vivek.kumar2 <vivek.kumar@ignitesol.com>
2019-07-31 13:16:47 +0530

    encapsulation added

--- naive_bayes.py
+++ naive_bayes.py
@@ -2,12 +2,12 @@ import pandas as pd
 
 class NaiveBayes():
     def __init__(self):
-        self.intermediate_dict = {}
+        self._intermediate_dict = {}
         self.class_probablity = {}
 
     def train(self, df):
         self._intermediate_class_data(df)
-        pass
+
     def predict(self, X):
         """
         Perform classification on a dictionary on a test vector X
@@ -27,14 +27,14 @@ class NaiveBayes():
                 output_prob[class_key] = self.class_probablity[class_key]
                 for key in row:
                     value = row[key]
-                    output_prob[class_key] *= self.intermediate_dict[key][value][class_key]
+                    output_prob[class_key] *= self._intermediate_dict[key][value][class_key]
                 if max_prob < output_prob[class_key]:
                     output_label = class_key
                     max_prob = output_prob[class_key]
             output_labels.append(output_label)
         return output_labels
 
-    def calc_class_probablity(self, df):
+    def _calc_class_probablity(self, df):
         intermediate_class_df = df.groupby(['Play-Tennis'])['Play-Tennis'].count()
         for class_col in intermediate_class_df.index.values:
             self.class_probablity[class_col] = intermediate_class_df[class_col]/intermediate_class_df.sum()
@@ -46,13 +46,13 @@ class NaiveBayes():
         for col in columns:
             int_df = df.groupby(['Play-Tennis', col])[col].count()
 
-            self.intermediate_dict[col] = {}
+            self._intermediate_dict[col] = {}
             for values in int_df.index.values.tolist():
-                if(values[1] not in self.intermediate_dict[col]):
-                    self.intermediate_dict[col][values[1]] = {}
+                if(values[1] not in self._intermediate_dict[col]):
+                    self._intermediate_dict[col][values[1]] = {}
                 class_wise_sum = int_df[values[0]].sum()
-                self.intermediate_dict[col][values[1]][values[0]] = int_df[values[0]][values[1]]/class_wise_sum
-        self.calc_class_probablity(df)
+                self._intermediate_dict[col][values[1]][values[0]] = int_df[values[0]][values[1]]/class_wise_sum
+        self._calc_class_probablity(df)
 
 if __name__ == "__main__":
     df = pd.read_csv("data/tennis_anyone.csv")
